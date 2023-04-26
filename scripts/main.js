35c70eac84e7bc28f858327567b2e17c049cc9ac
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-04-26 11:28:09 +0200

    Update active class and delete script to change active class

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-function setActive(htmlValue) {
-    var options = document.getElementsByClassName("options");
+// function setActive(htmlValue) {
+//     var options = document.getElementsByClassName("options");
 
-    for (let i = 0; i < options.length; i++) {
-        options[i].innerHTML.includes(htmlValue) ? options[i].classList.add("active") : options[i].classList.remove("active");
-    }
-}
+//     for (let i = 0; i < options.length; i++) {
+//         options[i].innerHTML.includes(htmlValue) ? options[i].classList.add("active") : options[i].classList.remove("active");
+//     }
+// }
