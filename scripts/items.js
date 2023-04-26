b8b7ae53ee697156e01e3234ecc351f7d7bae519
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-04-26 12:14:32 +0200

    Add html content and function to create each product

--- scripts/items.js
+++ scripts/items.js
@@ -10,3 +10,36 @@ function searchProducts(value) {
         }
     }
 }
+
+function createProduct(product){
+
+    var itemContainer = document.createElement("div");
+    itemContainer.classList.add("item");
+
+    var divImage = document.createElement("div");
+    divImage.classList.add("image");
+
+    var image = document.createElement("img");
+    image.src = "https://cdn.pixabay.com/photo/2014/04/03/11/07/bananas-311788_640.png";
+    image.alt = "Lo siento, este producto se ha encontrado :(";
+    image.classList.add("product-image");
+
+    var productName = document.createElement("div");
+    productName.classList.add("name");
+    productName.innerHTML = "Plátano";
+
+    var identification = document.createElement("div");
+    identification.classList.add("id");
+    identification.innerHTML = "1";
+
+    var description = document.createElement("div");
+    description.classList.add("description");
+
+    document.getElementById("items-list").appendChild(enlace);
+    enlace.appendChild(bicho);
+    itemContainer.appendChild(divImage);
+    itemContainer.appendChild(productName);
+    itemContainer.appendChild(identification);
+    itemContainer.appendChild(description);
+    divImage.appendChild(image);
+}
