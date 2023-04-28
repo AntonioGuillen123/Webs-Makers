367c4ef0748607b301b8631125db7ceab657ae72
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-04-28 12:30:01 +0200

    Add products style

--- scripts/items.js
+++ scripts/items.js
@@ -46,45 +46,63 @@ function createProduct(product) {
     image.alt = "Lo siento, este producto no se ha encontrado :(";
     image.classList.add("product-image");
 
-    var productName = document.createElement("div");
-    productName.classList.add("name");
+    var tableContainer = document.createElement("table");
+    var tableTitle = document.createElement("tr");
+    var tableValue = document.createElement("tr");
+    tableContainer.appendChild(tableTitle);
+    tableContainer.appendChild(tableValue);
 
-    var productNameTitle = document.createElement("p");
+    // var productName = document.createElement("div");
+    // productName.classList.add("name");
+
+    var productNameTitle = document.createElement("th");
     productNameTitle.innerHTML = "Nombre";
+    tableTitle.appendChild(productNameTitle);
 
-    var productNameValue = document.createElement("p");
+    var productNameValue = document.createElement("td");
     productNameValue.innerHTML = product.name;
+    tableValue.appendChild(productNameValue);
 
 
-    var identification = document.createElement("div");
-    identification.classList.add("id");
 
-    var identificationTitle = document.createElement("p");
+    // var identification = document.createElement("div");
+    // identification.classList.add("id");
+
+    var identificationTitle = document.createElement("th");
     identificationTitle.innerHTML = "ID";
+    tableTitle.appendChild(identificationTitle);
 
-    var identificationValue = document.createElement("p");
+
+    var identificationValue = document.createElement("td");
     identificationValue.innerHTML = "1";
+    tableValue.appendChild(identificationValue);
 
-    var description = document.createElement("div");
-    description.classList.add("description");
 
-    var descriptionTitle = document.createElement("p");
+    // var description = document.createElement("div");
+    // description.classList.add("description");
+
+    var descriptionTitle = document.createElement("th");
     descriptionTitle.innerHTML = "Descripción";
+    tableTitle.appendChild(descriptionTitle);
+    
 
-    var descriptionValue = document.createElement("p");
+    var descriptionValue = document.createElement("td");
     descriptionValue.innerHTML = product.description;
+    tableValue.appendChild(descriptionValue);
+
 
     itemContainer.appendChild(divImage);
-    itemContainer.appendChild(productName);
-    itemContainer.appendChild(identification);
-    itemContainer.appendChild(description);
+    // itemContainer.appendChild(productName);
+    // itemContainer.appendChild(identification);
+    // itemContainer.appendChild(description);
+    itemContainer.appendChild(tableContainer);
     divImage.appendChild(image);
-    productName.appendChild(productNameTitle);
-    productName.appendChild(productNameValue);
-    identification.appendChild(identificationTitle);
-    identification.appendChild(identificationValue);
-    description.appendChild(descriptionTitle);
-    description.appendChild(descriptionValue);
+    // productName.appendChild(productNameTitle);
+    // productName.appendChild(productNameValue);
+    // identification.appendChild(identificationTitle);
+    // identification.appendChild(identificationValue);
+    // description.appendChild(descriptionTitle);
+    // description.appendChild(descriptionValue);
 
     document.getElementById("items-list").appendChild(itemContainer);
 }
