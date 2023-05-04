6fff4010ff336d76024dffb998a44ca1543acfa7
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-05-04 02:12:31 +0200

    update general

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+var totalCart = 0;
+
 async function giveItems(){
     const response = await fetch("https://getpantry.cloud/apiv1/pantry/f05c7024-db22-4ef2-9691-d82f3c50cd0e/basket/items");
     const data = await response.json();
