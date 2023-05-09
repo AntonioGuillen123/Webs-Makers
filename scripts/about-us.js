43bab86d7eb16b3ab22bcacd6aef635adf7b322e
marinalp04 <arriagamarina4@gmail.com>
2023-05-09 18:14:29 +0200

    Update about-us.js

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,23 +1,30 @@
-var titulos = document.querySelectorAll("#name");
+var names = document.querySelectorAll("#name");
+var info = document.querySelectorAll("#personInfo");
+var first = document.getElementById("first");
 
-for(var i = 0; i < titulos.length; i++ ){
-    titulos[i].addEventListener("mouseover", function(){
+console.log(info)
+for(var i = 0; i < names.length; i++ ){
+    names[i].addEventListener("mouseover", function(){
         this.style.background = "#3554EE";
         this.style.color = "black";
 
     });
-    titulos[i].addEventListener("mouseout", function(){
+    names[i].addEventListener("mouseout", function(){
         this.style.background = "";
         this.style.color = "";
         
     })
 }
 
+for(var i = 0; i < names.length; i++){
+    names[i].addEventListener("click", function(){
+        first.parentNode.removeChild(first);
+        first = info[i];
+    })
+}
 
-var primerTitulo = document.getElementById("name");
 
-primerTitulo.addEventListener("click", function(){
-    var primerDiv = document.getElementById("personInfo");
-    primerDiv.style.display = "flex";
-})
+
+
+
 
