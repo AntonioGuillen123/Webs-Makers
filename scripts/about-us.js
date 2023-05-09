4f36ea9b07a16974b354eb7117d19069907396f7
marinalp04 <arriagamarina4@gmail.com>
2023-05-09 12:21:13 +0200

    Update about-us.js and about-us.html

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,42 +1,23 @@
-var div1 = document.getElementById("name");
-var div2 = document.getElementById("personInfo");
-// var elmMenu = document.getElementsByClassName("menu-item");
-// var i;
-// for (i = 0; i < elmMenu.length; i++) {
-//   elmMenu[i].style.backgroundColor = "#92a8d1";
-// }
+var titulos = document.querySelectorAll("#name");
 
-for (i = 0; i < div1.length; i++) {
-    function nameMouseOver(){
+for(var i = 0; i < titulos.length; i++ ){
+    titulos[i].addEventListener("mouseover", function(){
+        this.style.background = "#3554EE";
+        this.style.color = "black";
+
+    });
+    titulos[i].addEventListener("mouseout", function(){
+        this.style.background = "";
+        this.style.color = "";
         
-        div1[i].style.background = "#0404B4";
-        div1[i].style.color = "black"
-    
-    }
-    function nameMouseOut(){
-        div1[i].style.background = "rgb(75, 208, 238)";
-        div1[i].style.color = "white"
-    
-    }
-    
-    div1[i].addEventListener("mouseover", nameMouseOver);
-    div1[i].addEventListener("mouseout", nameMouseOut);
-    }
-
-
-
-function infoMouseOver(){
-    div2.style.background = "#A4A4A4";
-    div2.style.color = "black"
-
-}
-function infoMouseOut(){
-    div2.style.background = "black";
-    div2.style.color = "white"
-
+    })
 }
 
 
-div2.addEventListener("mouseover", infoMouseOver);
-div2.addEventListener("mouseout", infoMouseOut);
+var primerTitulo = document.getElementById("name");
+
+primerTitulo.addEventListener("click", function(){
+    var primerDiv = document.getElementById("personInfo");
+    primerDiv.style.display = "flex";
+})
 
