f5d49125ab47faad4a1ae60b008cfe646b19eac9
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-09 16:17:05 +0200

    update password

--- scripts/password.js
+++ scripts/password.js
@@ -7,7 +7,7 @@ function writePassword() {
     var tries = document.getElementById("tries");
     var fail = document.getElementById("failPassword");
     var formAdmin = document.getElementById("formAdmin");
-    var button = document.getElementById("submit");
+    //var button = document.getElementById("submit");
 
     if (input.value == truePassword) {
         window.location.href = "admin.html";
@@ -33,7 +33,7 @@ function writePassword() {
 }
 
 function removeVibration(formAdmin) {
-    setTimeout(function() {
+    setTimeout(() => {
             formAdmin.classList.remove("vibration");
           }, 500);
 }
