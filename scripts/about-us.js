d84f3e6671831bf202c1bb779329c727cdf4641f
marinalp04 <arriagamarina4@gmail.com>
2023-05-10 17:22:04 +0200

    update about-us.js

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -5,6 +5,7 @@ var first = document.getElementById("first");
 info[0].style.display = "block";
 
 
+
 for(var i = 0; i < botones.length; i++ ){
     botones[i].addEventListener("mouseover", function(){
         this.style.background = "#3554EE";
@@ -16,16 +17,18 @@ for(var i = 0; i < botones.length; i++ ){
         this.style.color = "";
         
     })
+    
 }
-console.log(info[1]);
+
 for(let i = 0; i < botones.length; i++){
     botones[i].addEventListener("click", function(){
         for(let j = 0; j < info.length; j++){
             console.log(info[j]);
             info[j].style.display = "none";
         }
-        console.log(i - 1);
+        console.log(i);
         info[i].style.display = "block";
+        
     })
 }
 
