4e88bf53e951e338174afd744b508afdbe2ba97f
marinalp04 <arriagamarina4@gmail.com>
2023-05-10 12:14:03 +0200

    Update about-us.js and about-us.html

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,25 +1,31 @@
-var names = document.querySelectorAll("#name");
-var info = document.querySelectorAll("#personInfo");
+var botones = document.querySelectorAll('button');
+var info = document.querySelectorAll(".contenido");
 var first = document.getElementById("first");
 
-console.log(info)
-for(var i = 0; i < names.length; i++ ){
-    names[i].addEventListener("mouseover", function(){
+info[0].style.display = "block";
+
+
+for(var i = 0; i < botones.length; i++ ){
+    botones[i].addEventListener("mouseover", function(){
         this.style.background = "#3554EE";
         this.style.color = "black";
 
     });
-    names[i].addEventListener("mouseout", function(){
+    botones[i].addEventListener("mouseout", function(){
         this.style.background = "";
         this.style.color = "";
         
     })
 }
-
-for(var i = 0; i < names.length; i++){
-    names[i].addEventListener("click", function(){
-        first.parentNode.removeChild(first);
-        first = info[i];
+console.log(info[1]);
+for(let i = 0; i < botones.length; i++){
+    botones[i].addEventListener("click", function(){
+        for(let j = 0; j < info.length; j++){
+            console.log(info[j]);
+            info[j].style.display = "none";
+        }
+        console.log(i - 1);
+        info[i].style.display = "block";
     })
 }
 
