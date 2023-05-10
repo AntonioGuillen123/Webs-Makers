31a910e1c72fada7cfc16e908ba41b923122a672
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2023-05-10 21:05:24 +0200

    Update about-us.js

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -6,32 +6,34 @@ info[0].style.display = "block";
 
 
 
-for(var i = 0; i < botones.length; i++ ){
-    botones[i].addEventListener("mouseover", function(){
+for (var i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("mouseover", function () {
         this.style.background = "#3554EE";
         this.style.color = "black";
 
     });
-    botones[i].addEventListener("mouseout", function(){
+    botones[i].addEventListener("mouseout", function () {
         this.style.background = "";
         this.style.color = "";
-        
+
     })
-    
+
 }
 
-for(let i = 0; i < botones.length; i++){
-    botones[i].addEventListener("click", function(){
-        for(let j = 0; j < info.length; j++){
-            console.log(info[j]);
-            info[j].style.display = "none";
-        }
-        console.log(i);
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", () => {
+        dontSee(info);
         info[i].style.display = "block";
-        
+
     })
+
 }
 
+function dontSee(element) {
+    for (let i = 0; i < element.length; i++) {
+        info[i].style.display = "none";
+    }
+}
 
 
 
