b2eba7d9cc81db97420a0b75114ffb0d6c5ed7aa
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-22 03:30:52 +0200

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -50,45 +50,47 @@ async function getUsers() {
 function viewCartUsers() {
     var users = document.getElementById("usersItems");
 
-    for (var i = 0; i < arrayCartUsers.length; i++) {
-        for (var j = 0; j < arrayCartUsers[i].length; j++) {
-            arrayCartUsers[i][j].id = j + 1;
+    if (arrayCartUsers[0] != undefined) {
+        for (var i = 0; i < arrayCartUsers.length; i++) {
+            for (var j = 0; j < arrayCartUsers[i].length; j++) {
+                arrayCartUsers[i][j].id = j + 1;
 
-            var user = document.createElement("div");
-            var userName = document.createElement("div");
-            var userDate = document.createElement("div");
-            var userId = document.createElement("div");
+                var user = document.createElement("div");
+                var userName = document.createElement("div");
+                var userDate = document.createElement("div");
+                var userId = document.createElement("div");
 
-            userId.innerHTML = `ID de Usuario: ${arrayCartUsers[i][j].id}`;
-            userName.innerHTML = `Usuario: ${arrayCartUsers[i][j].user}`;
-            userDate.innerHTML = `Fecha y hora: ${arrayCartUsers[i][j].date} - ${arrayCartUsers[i][j].time}`;
+                userId.innerHTML = `ID de Usuario: ${arrayCartUsers[i][j].id}`;
+                userName.innerHTML = `Usuario: ${arrayCartUsers[i][j].user}`;
+                userDate.innerHTML = `Fecha y hora: ${arrayCartUsers[i][j].date} - ${arrayCartUsers[i][j].time}`;
 
-            arrayCartItems.push(arrayCartUsers[i][j].items);
+                arrayCartItems.push(arrayCartUsers[i][j].items);
 
-            users.appendChild(user);
-            user.appendChild(userId);
-            user.appendChild(userName);
-            user.appendChild(userDate);
+                users.appendChild(user);
+                user.appendChild(userId);
+                user.appendChild(userName);
+                user.appendChild(userDate);
 
-            for (var k = 0; k < arrayCartItems.length; k++) {
-                var items = document.createElement("div");
-                for (var l = 0; l < arrayCartItems[k].length; l++) {
-                    var item = document.createElement("div");
-                    var itemAmount = document.createElement("div");
-                    var itemId = document.createElement("div");
-                    var itemName = document.createElement("div");
+                for (var k = 0; k < arrayCartItems.length; k++) {
+                    var items = document.createElement("div");
+                    for (var l = 0; l < arrayCartItems[k].length; l++) {
+                        var item = document.createElement("div");
+                        var itemAmount = document.createElement("div");
+                        var itemId = document.createElement("div");
+                        var itemName = document.createElement("div");
 
-                    itemId.innerHTML = `ID: ${arrayCartItems[k][l].id}`;
-                    itemName.innerHTML = `Nombre: ${arrayCartItems[k][l].name}`;
-                    itemAmount.innerHTML = `Cantidad: ${arrayCartItems[k][l].amount}`;
+                        itemId.innerHTML = `ID: ${arrayCartItems[k][l].id}`;
+                        itemName.innerHTML = `Nombre: ${arrayCartItems[k][l].name}`;
+                        itemAmount.innerHTML = `Cantidad: ${arrayCartItems[k][l].amount}`;
 
-                    item.appendChild(itemId);
-                    item.appendChild(itemName);
-                    item.appendChild(itemAmount);
-                    items.appendChild(item);
+                        item.appendChild(itemId);
+                        item.appendChild(itemName);
+                        item.appendChild(itemAmount);
+                        items.appendChild(item);
+                    }
                 }
+                user.appendChild(items);
             }
-            user.appendChild(items);
         }
     }
 }
@@ -96,29 +98,31 @@ function viewCartUsers() {
 function viewFormUsers() {
     var users = document.getElementById("usersContact");
 
-    for (var i = 0; i < arrayFormUsers.length; i++) {
-        for (var j = 0; j < arrayFormUsers[i].length; j++) {
-            arrayFormUsers[i][j].id = j + 1;
-
-            var user = document.createElement("div");
-            var userEmail = document.createElement("div");
-            var userAffair = document.createElement("div");
-            var userMessage = document.createElement("div");
-            var userDateTime = document.createElement("div");
-            var userId = document.createElement("div");
-
-            userId.innerHTML = `ID de Usuario: ${arrayFormUsers[i][j].id}`;
-            userEmail.innerHTML = `Email: ${arrayFormUsers[i][j].email}`;
-            userAffair.innerHTML = `Asunto: ${arrayFormUsers[i][j].affair}`
-            userMessage.innerHTML = `Mensaje: ${arrayFormUsers[i][j].message}`;
-            userDateTime.innerHTML = `Fecha y Hora: ${arrayFormUsers[i][j].date} - ${arrayFormUsers[i][j].time}`;
-
-            users.appendChild(user);
-            user.appendChild(userId);
-            user.appendChild(userEmail);
-            user.appendChild(userAffair);
-            user.appendChild(userMessage);
-            user.appendChild(userDateTime);
+    if (arrayFormUsers[0] != undefined) {
+        for (var i = 0; i < arrayFormUsers.length; i++) {
+            for (var j = 0; j < arrayFormUsers[i].length; j++) {
+                arrayFormUsers[i][j].id = j + 1;
+    
+                var user = document.createElement("div");
+                var userEmail = document.createElement("div");
+                var userAffair = document.createElement("div");
+                var userMessage = document.createElement("div");
+                var userDateTime = document.createElement("div");
+                var userId = document.createElement("div");
+    
+                userId.innerHTML = `ID de Usuario: ${arrayFormUsers[i][j].id}`;
+                userEmail.innerHTML = `Email: ${arrayFormUsers[i][j].email}`;
+                userAffair.innerHTML = `Asunto: ${arrayFormUsers[i][j].affair}`
+                userMessage.innerHTML = `Mensaje: ${arrayFormUsers[i][j].message}`;
+                userDateTime.innerHTML = `Fecha y Hora: ${arrayFormUsers[i][j].date} - ${arrayFormUsers[i][j].time}`;
+    
+                users.appendChild(user);
+                user.appendChild(userId);
+                user.appendChild(userEmail);
+                user.appendChild(userAffair);
+                user.appendChild(userMessage);
+                user.appendChild(userDateTime);
+            }
         }
     }
 }
@@ -137,7 +141,7 @@ function deleteAllMessages() {
     users.innerHTML = "";
 }
 
-function deleteUserShop() {
+async function deleteUserShop() {
     var form = document.getElementById("delete-shop");
     var input = document.getElementById("user-id-shop");
 
@@ -171,9 +175,11 @@ function deleteUserShop() {
     })
 
     console.log(newArrayCartUsers);
+
+    //await uploadUserShop(JSON.stringify(newArrayCartUsers));
 }
 
-function deleteUserMessage() {
+async function deleteUserMessage() {
     var form = document.getElementById("delete-message");
     var input = document.getElementById("user-id-message");
 
@@ -207,4 +213,26 @@ function deleteUserMessage() {
     })
 
     console.log(newArrayFormUsers);
+
+    //await uploadUsermessage(JSON.stringify(newArrayFormUsers));
+}
+
+async function uploadUserShop(data) {
+    await fetch("https://getpantry.cloud/apiv1/pantry/f05c7024-db22-4ef2-9691-d82f3c50cd0e/basket/cart-shopping", {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+}
+
+async function uploadUsermessage(data) {
+    await fetch("https://getpantry.cloud/apiv1/pantry/f05c7024-db22-4ef2-9691-d82f3c50cd0e/basket/form", {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
 }
