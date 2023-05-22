8ffcac066d2d2cf15b0f528de9f813976fac2412
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-22 03:38:40 +0200

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -176,7 +176,7 @@ async function deleteUserShop() {
 
     console.log(newArrayCartUsers);
 
-    //await uploadUserShop(JSON.stringify(newArrayCartUsers));
+    await uploadUserShop("hola"/* JSON.stringify(newArrayCartUsers) */);
 }
 
 async function deleteUserMessage() {
@@ -214,7 +214,7 @@ async function deleteUserMessage() {
 
     console.log(newArrayFormUsers);
 
-    //await uploadUsermessage(JSON.stringify(newArrayFormUsers));
+    await uploadUsermessage(JSON.stringify(newArrayFormUsers));
 }
 
 async function uploadUserShop(data) {
