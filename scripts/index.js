b7a2e5c4b2db83fd2a276e6e534f3a1d3c542389
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-05-22 23:06:04 +0200

    Update Index

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
 async function startIndex() {
+    enterAdmin();
+    await checkStock();
+}
+
+function enterAdmin() {
     var logo = document.getElementById("logoImg");
     var go = false;
 
@@ -6,12 +11,10 @@ async function startIndex() {
         go = true;
 
         logo.style.transform = "rotate(360deg)";
-        logo.style.webkitTransform = "rotate(360deg)";
 
         setTimeout(() => {
             if (go) {
                 logo.style.transform = "none";
-                logo.style.webkitTransform = "none"
 
                 setTimeout(() => {
                     window.location.href = "pages/password.html";
@@ -24,73 +27,73 @@ async function startIndex() {
         go = false;
 
         logo.style.transform = "none";
-        logo.style.webkitTransform = "none"
     });
 }
 
-fetch("https://getpantry.cloud/apiv1/pantry/f05c7024-db22-4ef2-9691-d82f3c50cd0e/basket/items")
-            .then((response) => response.json())
-            .then(response =>{
-                var divContainer = document.getElementById("fewUnits");
-                var title = document.getElementById("pocas");
-                var cont = 0;
-                for(var i = 0; i < response.items.length; i++){
-                    
-                    var id = response.items[i].id;
+async function checkStock() {
 
-                    // Te lo he cambiado porque antes basicamente lo que hacía es
-                    // que si el último era menor o mayor a 300 hacia lo que sea pero solo el último
-                    // de la coleccion porque se sobreescribía
-                    // Y este método es como el All de LINQ C#
-                    if(response.items[i].stock < 5){
-                        title.classList.add("title");
-                        title.innerHTML = "¡Quedan pocas unidades!";
+    var items = await giveItems();
 
-                        
-                        var a = document.createElement("a");
-                        a.setAttribute("href", `pages/items.html#${id}`);
-                        var divProduct = document.createElement("div");
-                        divProduct.classList.add("divProduct");
-                        a.classList.add("a");
-                        a.appendChild(divProduct);
-                        divContainer.appendChild(a);
+    var divContainer = document.getElementById("fewUnits");
+    var title = document.getElementById("pocas");
+    var cont = 0;
+
+    for (var i = 0; i < items.items.length; i++) {
+
+        var id = items.items[i].id;
+
+        // Te lo he cambiado porque antes basicamente lo que hacía es
+        // que si el último era menor o mayor a 300 hacia lo que sea pero solo el último
+        // de la coleccion porque se sobreescribía
+        // Y este método es como el All de LINQ C#
+        var stock = items.items[i].stock;
+
+        if (stock <= 5 && stock > 0) {
+            title.classList.add("title");
+            title.innerHTML = "¡Quedan pocas unidades!";
 
 
-                        var productName = response.items[i].name;
-                        var pName = document.createElement("p");
-                        pName.textContent = productName;
-                        pName.classList.add("productName");
-                        divProduct.appendChild(pName);
-
-                        var productPhoto = response.items[i].imageUrl;
-                        var img = document.createElement("img");
-                        img.src = productPhoto;
-                        img.classList.add("productImage");
-                        divProduct.appendChild(img);
-
-                        var productDescription = response.items[i].description;
-                        var pDescription = document.createElement("p");
-                        pDescription.textContent = productDescription;
-                        pDescription.classList.add("productDescription");
-                        divProduct.appendChild(pDescription);
-
-                        var productPrice = response.items[i].price;
-                        var pPrice = document.createElement("p");
-                        pPrice.textContent = "Precio por unidad: " + productPrice + "€";
-                        pPrice.classList.add("productPrice");
-                        divProduct.appendChild(pPrice);
-
-                        cont++;
+            var a = document.createElement("a");
+            a.setAttribute("href", `pages/items.html#${id}`);
+            var divProduct = document.createElement("div");
+            divProduct.classList.add("divProduct");
+            a.classList.add("a");
+            a.appendChild(divProduct);
+            divContainer.appendChild(a);
 
 
-                    }
-                    
-                }
-                if(cont === 0){
-                    var fullProducts = document.createElement("h3");
-                        fullProducts.innerHTML = "¡Qué suerte! Tenemos el stock lleno de todos los productos";
-                        fullProducts.classList.add("fullProducts");
-                        divContainer.appendChild(fullProducts);
-                }
+            var productName = items.items[i].name;
+            var pName = document.createElement("p");
+            pName.textContent = productName;
+            pName.classList.add("productName");
+            divProduct.appendChild(pName);
 
-            })
+            var productPhoto = items.items[i].imageUrl;
+            var img = document.createElement("img");
+            img.src = productPhoto;
+            img.classList.add("productImage");
+            divProduct.appendChild(img);
+
+            var productDescription = items.items[i].description;
+            var pDescription = document.createElement("p");
+            pDescription.textContent = productDescription;
+            pDescription.classList.add("productDescription");
+            divProduct.appendChild(pDescription);
+
+            var productPrice = items.items[i].price;
+            var pPrice = document.createElement("p");
+            pPrice.textContent = "Precio por unidad: " + productPrice + "€";
+            pPrice.classList.add("productPrice");
+            divProduct.appendChild(pPrice);
+
+            cont++;
+        }
+
+    }
+    if (cont === 0) {
+        var fullProducts = document.createElement("h3");
+        fullProducts.innerHTML = "¡Qué suerte! Tenemos el stock lleno de todos los productos";
+        fullProducts.classList.add("fullProducts");
+        divContainer.appendChild(fullProducts);
+    }
+}
